048672d956a7fa17c7213e60167283f411c43ae6
GifperTC <giftbbt@gmail.com>
2023-04-03 15:33:57 +0700

    create random quote function

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ var quotes = [
     "I'll torture you, kill you, revive you, and kill you again. -Natsume Sakasaki",
     "Only being referred to as a 'good boy' makes my ass itch, so I'd like to request a different term! - Ibara Saegusa",
     "In that case, I'd have rather gotten whipped! At least there's love! That way I could've thought of it as some kind of extreme play! But there wasn't any love, so it just hurt a lot! -Kaoru Hakaze",
-    "sir bible gay - Souma Kansaki",
+    "sir bible gay -Souma Kansaki",
     "die -Natsume Sakasaki" 
 ]
 
-var rannum = Math.floor(Math.random());
-console.log(rannum);
 function newQuote() {
-  
+  var rannum = Math.floor(Math.random() * quotes.length);
+  document.getElementById('quoteDisplay').innerHTML = quotes[rannum];
 }
