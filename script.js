c70efe5966e355ec9d72408ff5f58318a7996c40
GifperTC <giftbbt@gmail.com>
2023-04-03 15:22:01 +0700

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+var quotes = [
+    "I'll torture you, kill you, revive you, and kill you again. -Natsume Sakasaki",
+    "Only being referred to as a 'good boy' makes my ass itch, so I'd like to request a different term! - Ibara Saegusa",
+    "In that case, I'd have rather gotten whipped! At least there's love! That way I could've thought of it as some kind of extreme play! But there wasn't any love, so it just hurt a lot! -Kaoru Hakaze",
+    "sir bible gay - Souma Kansaki",
+    "die -Natsume Sakasaki" 
+]
+
+var rannum = Math.floor(Math.random());
+console.log(rannum);
+function newQuote() {
+  
+}
